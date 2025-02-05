2a71a00dcf5daf394a11e10aee672168457ebc2c
YYuuna <75922571+YYuuna@users.noreply.github.com>
2025-02-05 19:31:28 +0100

    setup for vercel deployment

--- MC_IT_challenge_2025/MC_IT_challenge_2025/settings.py
+++ MC_IT_challenge_2025/MC_IT_challenge_2025/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%apmticiwpv)zh6w9etacd#wpfs7)+6=fu6g7a=yc(rr5fqij5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
